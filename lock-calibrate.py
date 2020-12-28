e5c4db4db24204f0f4bdbba541b82967773a01ff
Sean House <33880135+sean-house@users.noreply.github.com>
2020-12-28 12:29:38 +0000

    Updates 28 Dec 2020

--- lock-calibrate.py
+++ lock-calibrate.py
@@ -9,25 +9,38 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
+# Defines the data bit that is transmitted preferentially in the shiftOut function.
+LSBFIRST = 1
+MSBFIRST = 2
+
 # GPIO Pins  GPIO.BOARD Numbering
 GPIO4 = 7
+GPIO17 = 11
 GPIO18 = 12
 GPIO16 = 36
 GPIO20 = 38
 GPIO21 = 40
+GPIO22 = 15
 GPIO23 = 16
 GPIO24 = 18
 GPIO25 = 22
+GPIO27 = 13
 
 lock_switch_pin = GPIO21
-lid_switch1_pin = GPIO16
-lid_switch2_pin = GPIO20
+req_switch_pin = GPIO4
+hinge_switch_pin = GPIO16
+lid_switch_pin = GPIO20
+dataPin = GPIO17  # DS Pin of 74HC595(Pin14)
+latchPin = GPIO27  # ST_CP Pin of 74HC595(Pin12)
+clockPin = GPIO22  # CH_CP Pin of 74HC595(Pin11)
 motorPins = (GPIO18, GPIO23, GPIO24, GPIO25)
 advance_step = (0x01, 0x02, 0x04, 0x08)  # define power supply order for coil for rotating anticlockwise
 retract_step = (0x08, 0x04, 0x02, 0x01)  # define power supply order for coil for rotating clockwise
 
+light_state = 0
 
-def setup_gpio():
+
+def setup_gpio(callback):
     """
 
     :return:
@@ -35,11 +48,16 @@ def setup_gpio():
     global p
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)  # Number GPIOs by its physical location
+    GPIO.setup(dataPin, GPIO.OUT)
+    GPIO.setup(latchPin, GPIO.OUT)
+    GPIO.setup(clockPin, GPIO.OUT)
     GPIO.setup(lock_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(lid_switch1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(lid_switch2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(hinge_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(lid_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(req_switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     for pin in motorPins:
         GPIO.setup(pin, GPIO.OUT)
+    GPIO.add_event_detect(req_switch_pin, GPIO.FALLING, callback=callback, bouncetime=600)
     return
 
 
@@ -73,24 +91,81 @@ def destroy_gpio():  # When 'Ctrl+C' is pressed, the function is executed.
 
 
 def lock_safe():
-    while any([GPIO.input(lid_switch2_pin) == GPIO.HIGH, GPIO.input(lid_switch1_pin) == GPIO.HIGH]):
+    while any([GPIO.input(hinge_switch_pin) == GPIO.HIGH, GPIO.input(lid_switch_pin) == GPIO.HIGH]):
         print('Close lid..', end='')
         time.sleep(0.5)
+    print('Locking now....')
     while GPIO.input(lock_switch_pin) == GPIO.HIGH:
         moveLock('A', 1)
     return
 
 
 def unlock_safe():
-    moveLock('R', 10)
+    moveLock('R', 16)
     return
+
+
+# shiftOut function, use bit serial transmission.
+def shift_bits(dPin, cPin, order, val):
+    for i in range(0, 8):
+        GPIO.output(cPin, GPIO.LOW);
+        if order == LSBFIRST:
+            GPIO.output(dPin, (0x01 & (val >> i) == 0x01) and GPIO.HIGH or GPIO.LOW)
+        elif order == MSBFIRST:
+            GPIO.output(dPin, (0x80 & (val << i) == 0x80) and GPIO.HIGH or GPIO.LOW)
+        GPIO.output(cPin, GPIO.HIGH)
+    return
+
+
+def set_lights(n: str):
+    """
+    Set the RPi lights to the parameters specified OFF, Green or 1-5 Red
+    :param n:
+    :return:
+    """
+    light_settings = {'OFF': 0x00,
+                      'G': 0x80,
+                      '1R': 0x40,
+                      '2R': 0x60,
+                      '3R': 0x70,
+                      '4R': 0x78,
+                      '5R': 0x7C,
+                      'ERR': 0x54}
+    GPIO.output(latchPin, GPIO.LOW) # Set 74HC595 to receive
+    shift_bits(dataPin, clockPin, LSBFIRST, light_settings[n])  # Shift the relevant definition byte to the 74HC595
+    GPIO.output(latchPin, GPIO.HIGH)    # Lock the 74HC595 / show the lights
+    return
+
+
+def button_pressed(channel):  # Interrupt call when button has been pressed
+    global light_state
+    seq = ['5R', '4R', '3R', '2R', '1R', 'OFF', 'G']
+    light_state += 1
+    light_state = light_state % 7
+    print('Setting lights to {}'.format(seq[light_state]))
+    set_lights(seq[light_state])
+    return
+
+
+def spin_lights():
+    seq = ['OFF', '5R', '4R', '3R', '2R', '1R', 'OFF', '1R', '2R', '3R', '4R', '5R',
+           'ERR', 'OFF', 'ERR', 'OFF', 'G', 'OFF', 'G', 'OFF']
+    for i in seq:
+        set_lights(i)
+        time.sleep(0.1)
+    print('Diagnostic message:  Spin lights')
+
 
 if __name__ == '__main__':  # Program starting from here
     print('Program is starting...')
-    setup_gpio()
+    setup_gpio(button_pressed)
+    spin_lights()
     try:
         while True:
-            print('Lock switch closed = {}, {}, {}'.format(GPIO.input(lock_switch_pin), GPIO.input(lid_switch1_pin), GPIO.input(lid_switch2_pin)))
+            print('Lock switch closed = {}'.format(GPIO.input(lock_switch_pin)))
+            print('Hinge switch closed = {}'.format(GPIO.input(hinge_switch_pin)))
+            print('Lid switch closed = {}'.format(GPIO.input(lid_switch_pin)))
+            print('Request switch closed = {}'.format(GPIO.input(req_switch_pin)))
             i = input('?')
             if i == 'r':
                 moveLock('R', 1)
